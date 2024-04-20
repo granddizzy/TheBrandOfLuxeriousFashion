e47d6dbca3408890956700c01d785a5cd30821fc
roman lepihov <lepihovrom@yandex.by>
2024-04-20 15:05:40 +0300

    add json parse

---
+++ js/indexJson.js
@@ -0,0 +1,40 @@
+const feturedItems = `{
+  "feturedItems": [
+    {
+      "photo": "./img/fetured1.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, totam?",
+      "price": "52.00"
+    },
+    {
+      "photo": "./img/fetured2.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, saepe?",
+      "price": "62.00"
+    },
+    {
+      "photo": "./img/fetured3.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, magnam!",
+      "price": "72.00"
+    },
+    {
+      "photo": "./img/fetured4.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, soluta?",
+      "price": "82.00"
+    },
+    {
+      "photo": "./img/fetured5.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, provident.",
+      "price": "92.00"
+    },
+    {
+      "photo": "./img/fetured6.png",
+      "title": "ELLERY X M'O CAPSULE",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, modi.",
+      "price": "102.00"
+    }
+  ]
+}`;
--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
+"use strickt";
+
+// menu
 const menu = document.querySelector(".mainMenu");
 const burger = document.querySelector(".buttonMenu");
 
 function toggleMenu() {
-    menu.classList.toggle("active");
+  menu.classList.toggle("active");
 }
 
 burger.addEventListener("click", toggleMenu);
+
+document.querySelector('#catalogButton').addEventListener('click', function () {
+  window.location.href = 'catalog.html';
+});
+
+// fetured Items
+const feturedItemsObj = JSON.parse(feturedItems);
+const templateFeturedItemEl = document.querySelector('#templateFeturedItem').content;
+const catalogItemsEl = document.querySelector('div.catalogItems');
+
+feturedItemsObj.feturedItems.forEach(item => {
+  const clone = document.importNode(templateFeturedItemEl, true);
+  clone.querySelector('div.catalogItem__photo').firstElementChild.src = item.photo;
+  clone.querySelector('h3.catalogItem__title').textContent = item.title;
+  clone.querySelector('p.catalogItem__description').textContent = item.description;
+  clone.querySelector('p.catalogItem__price').textContent = item.price;
+
+  catalogItemsEl.appendChild(clone);
+})
+
+document.querySelectorAll('div.catalogItem').forEach(div => {
+  div.addEventListener('click', function() {
+    window.location.href = 'product.html';
+  });
+});
