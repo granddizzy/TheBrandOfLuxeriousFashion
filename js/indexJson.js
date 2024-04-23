27d813d09f3dcaa1e4e6370ea5481a345acacefe
roman lepihov <lepihovrom@yandex.by>
2024-04-23 16:25:10 +0300

    add index cart

--- js/indexJson.js
+++ js/indexJson.js
@@ -1,36 +1,42 @@
 const feturedItems = `{
   "feturedItems": [
     {
+      "id": 1,
       "photo": "./img/fetured1.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, totam?",
       "price": "52.00"
     },
     {
+      "id": 2,
       "photo": "./img/fetured2.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, saepe?",
       "price": "62.00"
     },
     {
+      "id": 3,
       "photo": "./img/fetured3.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, magnam!",
       "price": "72.00"
     },
     {
+      "id": 4,
       "photo": "./img/fetured4.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, soluta?",
       "price": "82.00"
     },
     {
+      "id": 5,
       "photo": "./img/fetured5.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, provident.",
       "price": "92.00"
     },
     {
+      "id": 6,
       "photo": "./img/fetured6.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, modi.",
--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ const catalogItemsEl = document.querySelector('div.catalogItems');
 
 feturedItemsObj.feturedItems.forEach(item => {
   const clone = document.importNode(templateFeturedItemEl, true);
+  clone.querySelector('div.catalogItem').setAttribute('data-id', item.id);
   clone.querySelector('div.catalogItem__photo').firstElementChild.src = item.photo;
   clone.querySelector('h3.catalogItem__title').textContent = item.title;
   clone.querySelector('p.catalogItem__description').textContent = item.description;
@@ -29,8 +30,28 @@ feturedItemsObj.feturedItems.forEach(item => {
   catalogItemsEl.appendChild(clone);
 })
 
-document.querySelectorAll('div.catalogItem').forEach(div => {
-  div.addEventListener('click', function() {
+document.querySelector('div.catalogItems').addEventListener('click', function (e) {
+  // Метод closest() возвращает ближайший родительский элемент, который соответствует указанному селектору, или null,
+  //   если такой элемент не найден.
+  if (e.target.closest('div.catalogItem__button')) {
+    const itemId = e.target.closest('div.catalogItem').getAttribute('data-id');
+    addCartItem(itemId);
+  } else {
     window.location.href = 'product.html';
-  });
-});
+  }
+})
+
+document.querySelector('div.shoppingCart').addEventListener('click', function (e) {
+  if (e.target.classList.contains('shoppingItem__crossButton')) {
+    const itemId = e.target.closest('div.shoppingItem').getAttribute('data-id')
+    delCartItem(itemId);
+  }
+})
+
+function addCartItem(itemId) {
+
+}
+
+function delCartItem(itemId) {
+
+}
