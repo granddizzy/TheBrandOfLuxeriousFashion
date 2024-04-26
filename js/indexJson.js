1d2eed58a3d8429399a0b3ede68964b7b1ed1857
roman lepihov <lepihovrom@yandex.by>
2024-04-26 21:42:03 +0300

    update

--- js/indexJson.js
+++ js/indexJson.js
@@ -1,46 +1,58 @@
-const feturedItems = `{
-  "feturedItems": [
+const jsonData = `{
+  "items": [
     {
       "id": 1,
       "photo": "./img/fetured1.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, totam?",
-      "price": "52.00"
+      "price": "52.00",
+      "color": "Red",
+      "size": "L"
     },
     {
       "id": 2,
       "photo": "./img/fetured2.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, saepe?",
-      "price": "62.00"
+      "price": "62.00",
+      "color": "Green",
+      "size": "M"
     },
     {
       "id": 3,
       "photo": "./img/fetured3.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, magnam!",
-      "price": "72.00"
+      "price": "72.00",
+      "color": "Blue",
+      "size": "XL"
     },
     {
       "id": 4,
       "photo": "./img/fetured4.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, soluta?",
-      "price": "82.00"
+      "price": "82.00",
+      "color": "Yellow",
+      "size": "S"
     },
     {
       "id": 5,
       "photo": "./img/fetured5.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, provident.",
-      "price": "92.00"
+      "price": "92.00",
+      "color": "Orange",
+      "size": "XS"
     },
     {
       "id": 6,
       "photo": "./img/fetured6.png",
       "title": "ELLERY X M'O CAPSULE",
       "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, modi.",
-      "price": "102.00"
+      "price": "102.00",
+      "color": "red",
+      "size": "XL"
     }
   ]
 }`;
--- js/script.js
+++ js/script.js
@@ -14,44 +14,6 @@ document.querySelector('#catalogButton').addEventListener('click', function () {
   window.location.href = 'catalog.html';
 });
 
-// fetured Items
-const feturedItemsObj = JSON.parse(feturedItems);
-const templateFeturedItemEl = document.querySelector('#templateFeturedItem').content;
-const catalogItemsEl = document.querySelector('div.catalogItems');
+const jsonObj = JSON.parse(jsonData);
 
-feturedItemsObj.feturedItems.forEach(item => {
-  const clone = document.importNode(templateFeturedItemEl, true);
-  clone.querySelector('div.catalogItem').setAttribute('data-id', item.id);
-  clone.querySelector('div.catalogItem__photo').firstElementChild.src = item.photo;
-  clone.querySelector('h3.catalogItem__title').textContent = item.title;
-  clone.querySelector('p.catalogItem__description').textContent = item.description;
-  clone.querySelector('p.catalogItem__price').textContent = item.price;
-
-  catalogItemsEl.appendChild(clone);
-})
-
-document.querySelector('div.catalogItems').addEventListener('click', function (e) {
-  // Метод closest() возвращает ближайший родительский элемент, который соответствует указанному селектору, или null,
-  //   если такой элемент не найден.
-  if (e.target.closest('div.catalogItem__button')) {
-    const itemId = e.target.closest('div.catalogItem').getAttribute('data-id');
-    addCartItem(itemId);
-  } else {
-    window.location.href = 'product.html';
-  }
-})
-
-document.querySelector('div.shoppingCart').addEventListener('click', function (e) {
-  if (e.target.classList.contains('shoppingItem__crossButton')) {
-    const itemId = e.target.closest('div.shoppingItem').getAttribute('data-id')
-    delCartItem(itemId);
-  }
-})
-
-function addCartItem(itemId) {
-
-}
-
-function delCartItem(itemId) {
-
-}
+const shoppingCart = new Map();
